b3d069a4d3024e8f9b18254544131afdb830e5bb
Tim <tokhirov070714@gmail.com>
2025-11-02 14:37:12 +0500

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import { useEffect } from "react";
-import { Button } from "./components/ui/button";
 import { Routes, Route } from "react-router";
-import About from "./components/pages/Footer";
 import Layout from "./components/layouts/Layout";
 import Main from "./components/pages/Main";
 
 function App() {
 
-  // const spotify = {
+  const spotify = {
 
-  //   client_id: "bb8d6ef33bf34acb89de729548c86f2b",
-  //   REDIRECT_URI: "http://127.0.0.1:5173/callback",
-  //   AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
-  //   RESPONSE_TYPE: "token",
-  //   token: ""
+    client_id: "bb8d6ef33bf34acb89de729548c86f2b",
+    REDIRECT_URI: "http://192.168.100.78:5173/callback",
+    AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
+    RESPONSE_TYPE: "token",
+    token: "",
 
-  // }
+  }
+
+
 
   return (
 
@@ -26,8 +26,6 @@ function App() {
         <Route path="/" element={<Layout />}>
 
           <Route index element={<Main />} />
-
-          <Route path="/about" element={<About />} />
 
         </Route>
 
--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -2,9 +2,74 @@ import type React from "react"
 import { Outlet } from "react-router";
 import Header from "../pages/Header";
 import Footer from "../pages/Footer";
+import { useEffect } from "react";
 
 
 const Layout: React.FC = () => {
+
+
+    const spotify = {
+
+        client_id: "bb8d6ef33bf34acb89de729548c86f2b",
+        REDIRECT_URI: "http://192.168.100.78:5173/callback",
+        AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
+        RESPONSE_TYPE: "token",
+        token: "",
+
+    }
+
+    async function fetchUserData(token: string) {
+
+        try {
+
+            const res = await fetch("https://api.spotify.com/v1/me/", {
+
+                headers: {
+
+                    Authorization: `Bearer ${token}`
+
+                }
+
+            })
+
+            if (!res.ok) throw new Error(res.statusText)
+
+            const data = await res.json()
+
+            console.log(data);
+
+
+        } catch (e) {
+
+            console.log(e);
+
+
+        }
+
+    }
+
+    useEffect(() => {
+        const hash = window.location.hash;
+        let token = window.localStorage.getItem("token");
+
+        if (!token && hash) {
+            token = hash
+                ?.substring(1)
+                ?.split("&")
+                ?.find((elem) => elem.startsWith("access_token"))
+                ?.split("=")[1] ?? null;
+
+            window.location.href = "";
+            window.localStorage.setItem("token", token || "");
+        }
+
+        if (token) {
+
+            fetchUserData(token)
+
+        }
+
+    }, []);
 
     return (
 
@@ -12,7 +77,7 @@ const Layout: React.FC = () => {
 
         <div className="my-0 mx-auto relative">
 
-            <Header />
+            {/* <Header />
 
             <main>
 
@@ -20,7 +85,11 @@ const Layout: React.FC = () => {
 
             </main>
 
-            <Footer />
+            <Footer /> */}
+
+            <a href={`${spotify.AUTH_ENDPOINT}?client_id=${spotify.client_id}&redirect_uri=${spotify.REDIRECT_URI}&response_type=${spotify.RESPONSE_TYPE}&scope=playlist-modify-public`}>
+                <button className='log-in-btn'>Log in</button>
+            </a>
 
         </div>
 
